8c51a1700a0a321e5c47052611139f63f122a836
Keerthy Baby <keerthybaby025@gmail.com>
2023-12-17 21:25:43 +0530

    useReducer exercise

---
+++ useReducer/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import ReducerTutorial from "./ReducerTutorial";
+function App() {
+  return (
+    <div className="App">
+      <ReducerTutorial />
+    </div>
+  );
+}
+
+export default App;
