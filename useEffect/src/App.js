7ff8bd885641699d08a0b45291bc1eac9090ae25
Keerthy Baby <keerthybaby025@gmail.com>
2023-12-17 21:26:03 +0530

    useEffect exercise

---
+++ useEffect/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import EffectTutorial from "./EffectTutorial";
+function App() {
+  return (
+    <div className="App">
+      <EffectTutorial />
+    </div>
+  );
+}
+
+export default App;
