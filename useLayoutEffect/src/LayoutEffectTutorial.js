0756d48e3b78c67863fab960f4c336d125facc5c
Keerthy Baby <keerthybaby025@gmail.com>
2023-12-19 09:11:55 +0530

    useLayoutEffect exercise

---
+++ useLayoutEffect/src/LayoutEffectTutorial.js
@@ -0,0 +1,21 @@
+import React, { useLayoutEffect, useEffect, useRef } from "react";
+
+const LayoutEffectTutorial = () => {
+  const inputRef = useRef(null);
+
+  useLayoutEffect(() => {
+    console.log(inputRef.current.value);
+  }, []);
+
+  useEffect(() => {
+    inputRef.current.value = "HELLO";
+  }, []);
+
+  return (
+    <div>
+      <input ref={inputRef} value="PEDRO" style={{ width: 400, height: 60 }} />
+    </div>
+  );
+};
+
+export default LayoutEffectTutorial;
