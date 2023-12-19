d929f64a3054adbd826174e467f45dde75a84703
Keerthy Baby <keerthybaby025@gmail.com>
2023-12-19 09:12:27 +0530

    useRef exercise

---
+++ useRef/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import RefTutorial from "./RefTutorial";
+function App() {
+  return (
+    <div className="App">
+      <RefTutorial />
+    </div>
+  );
+}
+
+export default App;
