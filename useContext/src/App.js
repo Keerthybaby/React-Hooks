1b9a3e40485253821bda192c2123fdde05284aae
Keerthy Baby <keerthybaby025@gmail.com>
2023-12-20 23:04:44 +0530

    useContext exercise

---
+++ useContext/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import ContextTutorial from "./ContextTutorial";
+function App() {
+  return (
+    <div className="App">
+      <ContextTutorial />
+    </div>
+  );
+}
+
+export default App;
