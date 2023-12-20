471bff618a04f2b22a82f543ef829bc49b1e4653
Keerthy Baby <keerthybaby025@gmail.com>
2023-12-20 23:03:57 +0530

    useMemo exercise

---
+++ useMemo/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import MemoTutorial from "./MemoTutorial";
+function App() {
+  return (
+    <div className="App">
+      <MemoTutorial />
+    </div>
+  );
+}
+
+export default App;
